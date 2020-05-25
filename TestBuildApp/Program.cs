160fa17a396a580981963965db80dc220e6c6508
Ponnusamy <PONNUSVX@legal.regn.net>
2020-05-24 23:28:28 -0400

    Add project files.

---
+++ TestBuildApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace TestBuildApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App Started " + DateTime.Now.ToString());
+
+            for(int i =0; i < 10; i++)
+            {
+                Console.WriteLine("Processing: " + i + " - " + DateTime.Now.ToString());
+                Thread.Sleep(3000);
+            }
+
+            Console.WriteLine("App Completed " + DateTime.Now.ToString());
+        }
+    }
+}
